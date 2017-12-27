4888180e5aa79c10a667fbed691010952a568020
John <johnwilles@gmail.com>
2017-12-27 15:22:43 -0500

    Add movie list parsing

--- omdb_scrape.py
+++ omdb_scrape.py
@@ -1,31 +1,61 @@
 import requests
 import os
+import csv 
+import sys
 
+def send_request(movies):
 
-def send_request(title):
-    # Request
-    apiKey = os.environ['apiKey']
-    omdb_url = 'http://www.omdbapi.com'
+  apiKey = os.environ['apiKey']
+  omdb_url = 'http://www.omdbapi.com'
 
+  
+  for idx, movie in enumerate(movies):
     try:
         response = requests.get(
             url= omdb_url,
             params={
                 "apikey": apiKey,
-		"t": title
+        	"t": movie[2]
             }
         )
         print('Response HTTP Status Code: {status_code}'.format(
             status_code=response.status_code))
-        print('Response HTTP Response Body: {content}'.format(
-            content=response.content))
+#        print('Response HTTP Response Body: {content}'.format(
+#            content=response.content))
     except requests.exceptions.RequestException:
         print('HTTP Request failed')
 
-    return response.content
+    movies[idx].append(response.content)
+
+  return movies
+
+
+def read_movie_list(filename):
+ 
+  movies = []
+ 
+  f = open(filename, 'rb')
+  reader = csv.reader(f)
+
+  for row in reader:
+    movies.append(row)
+  f.close()
+ 
+  return movies  
 
 
 if __name__ == '__main__':
-  
-  print send_request("ratatouille")
+
+  movie_list = '../data/netflix-prize-data/movie_titles.csv'
+  movies = read_movie_list(movie_list)
+
+  print len(movies) 
+
+  print movies[:2]
+
+  print send_request(movies[:2])
+
+ # for movie in movies:
+#	print movie
+  #print send_request("ratatouille")
  
