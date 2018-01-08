8c1429e0b7df91c2bba98d55c457d8b04d71cd27
John <johnwilles@gmail.com>
2018-01-07 23:22:13 -0500

    Add 2016 data

--- scripts/omdb_scrape.py
+++ scripts/omdb_scrape.py
@@ -74,12 +74,15 @@ def read_movie_txt(filename):
 if __name__ == '__main__':
 
   netflix_movie_csv = '../data/netflix-prize-data/movie_titles.csv'
-  movies_2017_list = './movies_2017.txt'
+  #movies_2017_list = './movies_2017.txt'
+  movies_2016_list = '../data/movies_2016.txt'
   #movies = read_movie_csv(netflix_movie_list)
 
-  movies_2017 = read_movie_txt(movies_2017_list)
+  #movies_2017 = read_movie_txt(movies_2017_list)
+  movies_2016 = read_movie_txt(movies_2016_list)
 
-  movie_features = send_request(movies_2017)
+  #movie_features = send_request(movies_2017)
+  movie_features = send_request(movies_2016)
 
 
   movie_features_csv = [['Title', 'Year', 'Rated', 'Genre', 'Director', 'Actors', 'imdbRating']]
@@ -88,7 +91,7 @@ if __name__ == '__main__':
     movie_features_csv.append(movie)
 
 
-  with open("features.csv", "wb") as f:
+  with open("features_2016.csv", "wb") as f:
     writer = csv.writer(f)
     writer.writerows(movie_features_csv)
 
